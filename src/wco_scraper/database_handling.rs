c27dec08ad20f01a8cc2802f911340fd52efe67c
direshawarma <hannahqvalentine@gmail.com>
2022-06-04 01:58:24 -0500

    refactor, create new db, new db names/columns

--- src/wco_scraper/database_handling.rs
+++ src/wco_scraper/database_handling.rs
@@ -16,7 +16,8 @@ struct Episode {
 
 pub fn db_add(seriestitle: &String, title: String, url: String) {
     let dbconn = Connection::open("db.db").unwrap();
-    db_create(&dbconn); // if necessary
+    series_db_create(&dbconn);
+    episode_db_create(&dbconn);
 
 
     lazy_static! {
@@ -50,14 +51,14 @@ pub fn db_add(seriestitle: &String, title: String, url: String) {
         break;
     }
 
-    entry.eptitle = SPACEKILLER.replace(&entry.eptitle, "$before $after").to_string();
+    entry.series = SPACEKILLER.replace(&entry.eptitle, "$before $after").to_string();
     entry.eptitle = BEGINFIXER.replace(&entry.eptitle, |caps: &Captures| { format!("{}", &caps[1]) }).to_string();
     // FIXME things still don't wind up quite right with entry.eptitle using simple regex
     //  TODO make a more robust way of figuring out episode title names and removing fluff
 
 
     match dbconn.execute(
-        "INSERT INTO series (series, url, eptitle, scraped, season, episode) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+        "INSERT INTO episodes (series, url, eptitle, scraped, season, episode) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
         params![&entry.series, &entry.url, &entry.eptitle, &entry.scraped, &entry.season, &entry.episode],
     ) {
         Err(rusqlite::Error::SqliteFailure(errnum, errmsg)) => {
@@ -70,9 +71,30 @@ pub fn db_add(seriestitle: &String, title: String, url: String) {
     };
 }
 
-fn db_create(connection:&Connection) {
-    match connection.execute(
-        "
+
+// TODO find out if a create check or insert check are as fast as directly checking or not because this gets called for every episode read which might give us some I/O issues
+fn series_db_create(connection: &Connection) { // TODO use rowid is a much faster, functionally equivalent substitute for actually creating an autoincrementing index so implement rowid
+    match connection.execute("
+        CREATE TABLE series (
+            series TEXT,
+            url TEXT NOT NULL PRIMARY KEY,
+            last_retrieved FLOAT,
+            urltype TEXT,
+            parentrow INT
+    )", params![],
+    ) {
+        Err(rusqlite::Error::SqliteFailure(errnum, errmsg)) => {
+            if errnum.extended_code.eq(&1) && errmsg.as_ref().unwrap() == "table series already exists" { println!("Table series already exists") } else { panic!("Error -> {:?}: {:?}", errnum, errmsg); }
+        },
+        Ok(_) => { println!("series db init ok") },
+        Err(e) => {
+            panic!("Error: {}", e.to_string());
+        }
+    };
+}
+
+fn episode_db_create(connection: &Connection) {
+    match connection.execute("
         CREATE TABLE episodes (
             series TEXT,
             url TEXT NOT NULL PRIMARY KEY,
@@ -83,9 +105,9 @@ fn db_create(connection:&Connection) {
     )", params![],
     ) {
         Err(rusqlite::Error::SqliteFailure(errnum, errmsg)) => {
-            if errnum.extended_code.eq(&1) { () } else { panic!("Error -> {:?}: {:?}", errnum, errmsg); }
+            if errnum.extended_code.eq(&1) && errmsg.as_ref().unwrap() == "table episodes already exists" { println!("Table episodes already exists") } else { panic!("Error -> {:?}: {:?}", errnum, errmsg); }
         },
-        Ok(_) => { () },
+        Ok(_) => { println!("episode db init ok") },
         Err(e) => {
             panic!("Error: {}", e.to_string());
         }
