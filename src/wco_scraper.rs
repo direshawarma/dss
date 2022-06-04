3877f2937ea471f256e5224585f2cfc8df494cde
direshawarma <hannahqvalentine@gmail.com>
2022-06-04 00:11:17 -0500

    whitespace

--- src/wco_scraper.rs
+++ src/wco_scraper.rs
@@ -6,7 +6,7 @@ use scraper::{Html, Selector};
 mod database_handling;
 
 pub fn scrape(url: &str) {
-    println!("In file {}", url); // TODO convert this to use downloaded html instead of a local file. Keep local file handling for testing so we don't abuse the servers we scrape from
+    println!("In file {}", url);
 
     let html = fs::read_to_string(url)
         .expect("Something went wrong reading the file");
@@ -22,7 +22,7 @@ pub fn scrape(url: &str) {
         a.push(each)
     }
 
-    for element in &a { // todo figure out how to get .rev() to work on this without DoubleEndedIterator so that we can reverse order the element items before processing them
+    for element in &a {
         let url = element.value().attr("href").unwrap().to_string();
         let title = element.value().attr("title").unwrap().to_string();
         database_handling::db_add(&showtitle, title, url);
