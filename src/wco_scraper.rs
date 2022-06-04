3f628bb67f9ad89b0a28db4d785c62577cc9a9eb
direshawarma <hannahqvalentine@gmail.com>
2022-06-04 00:57:13 -0500

    variable refactor for clarity's sake

--- src/wco_scraper.rs
+++ src/wco_scraper.rs
@@ -13,9 +13,9 @@ pub fn scrape(url: &str) {
     let document = Html::parse_document(&html);
     let a_selector = Selector::parse("div.cat-eps > a.sonra").unwrap();
     let showtitle_selector = Selector::parse(".video-title > h1 > div.h1-tag > a").unwrap();
-    let mut showtitle:String = "".to_string();
-    for show in document.select(&showtitle_selector) {
-        showtitle = show.inner_html().to_string();
+    let mut seriestitle: String = "".to_string();
+    for series in document.select(&showtitle_selector) {
+        seriestitle = series.inner_html().to_string();
     }
     let mut a = Vec::new();
     for each in document.select(&a_selector) {
@@ -25,6 +25,6 @@ pub fn scrape(url: &str) {
     for element in &a {
         let url = element.value().attr("href").unwrap().to_string();
         let title = element.value().attr("title").unwrap().to_string();
-        database_handling::db_add(&showtitle, title, url);
+        database_handling::db_add(&seriestitle, title, url);
     }
 }
--- src/wco_scraper/database_handling.rs
+++ src/wco_scraper/database_handling.rs
@@ -6,7 +6,7 @@ use rusqlite::{Connection, params};
 
 #[derive(Debug)]
 struct Episode {
-    show: String,
+    series: String,
     url: String,
     eptitle: String,
     scraped: f64,
@@ -14,7 +14,7 @@ struct Episode {
     season: i32, // Someday Sesame Street might manage to break this limit
 }
 
-pub fn db_add(showtitle:&String,title:String,url:String) {
+pub fn db_add(seriestitle: &String, title: String, url: String) {
     let dbconn = Connection::open("db.db").unwrap();
     db_create(&dbconn); // if necessary
 
@@ -27,11 +27,11 @@ pub fn db_add(showtitle:&String,title:String,url:String) {
     }
     ;
 
-    let string = format!("{} {}", r"Watch", &showtitle);
+    let string = format!("{} {}", r"Watch", &seriestitle);
     let title_killer: Regex = Regex::new(&string).unwrap();
 
     let mut entry = Episode {
-        show: showtitle.to_string(),
+        series: seriestitle.to_string(),
         url: url.to_string(),
         eptitle: title_killer.replace(title.as_str(), "$after").to_string(),
         scraped: SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs_f64(),
@@ -41,22 +41,24 @@ pub fn db_add(showtitle:&String,title:String,url:String) {
 
     for cap in SEASONREGEX.captures_iter(&entry.eptitle.to_string()) {
         entry.season = cap[1].parse::<i32>().unwrap();
-        entry.eptitle = SEASONREGEX.replace(&entry.eptitle,"$before $after").to_string();
+        entry.eptitle = SEASONREGEX.replace(&entry.eptitle, "$before $after").to_string();
         break;
     }
     for cap in EPISODEREGEX.captures_iter(&entry.eptitle.to_string()) {
         entry.episode = cap[1].parse::<i32>().unwrap();
-        entry.eptitle = EPISODEREGEX.replace(&entry.eptitle,"$before $after").to_string();
+        entry.eptitle = EPISODEREGEX.replace(&entry.eptitle, "$before $after").to_string();
         break;
     }
 
-    entry.eptitle = SPACEKILLER.replace(&entry.eptitle,"$before $after").to_string();
-    entry.eptitle = BEGINFIXER.replace(&entry.eptitle,|caps: &Captures| {format!("{}",&caps[1])}).to_string();
+    entry.eptitle = SPACEKILLER.replace(&entry.eptitle, "$before $after").to_string();
+    entry.eptitle = BEGINFIXER.replace(&entry.eptitle, |caps: &Captures| { format!("{}", &caps[1]) }).to_string();
+    // FIXME things still don't wind up quite right with entry.eptitle using simple regex
+    //  TODO make a more robust way of figuring out episode title names and removing fluff
 
 
     match dbconn.execute(
-        "INSERT INTO shows (show, url, eptitle, scraped, season, episode) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
-        params![&entry.show, &entry.url, &entry.eptitle, &entry.scraped, &entry.season, &entry.episode],
+        "INSERT INTO series (series, url, eptitle, scraped, season, episode) VALUES (?1, ?2, ?3, ?4, ?5, ?6)",
+        params![&entry.series, &entry.url, &entry.eptitle, &entry.scraped, &entry.season, &entry.episode],
     ) {
         Err(rusqlite::Error::SqliteFailure(errnum, errmsg)) => {
             if errnum.extended_code.eq(&1555) { () } else { panic!("Error -> {:?}: {:?}", errnum, errmsg); }
@@ -71,8 +73,8 @@ pub fn db_add(showtitle:&String,title:String,url:String) {
 fn db_create(connection:&Connection) {
     match connection.execute(
         "
-        CREATE TABLE shows (
-            show TEXT,
+        CREATE TABLE episodes (
+            series TEXT,
             url TEXT NOT NULL PRIMARY KEY,
             eptitle TEXT,
             scraped FLOAT,
